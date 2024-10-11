45d129f88c160201b3ae0067d4947aad9b838c7e
DaffaMuhamad <Daffamuhamadra@gmail.com>
2024-10-11 15:56:27 +0700

    Video #11 OOP: Static Keyword

---
+++ OOP/Video#11/Static.php
@@ -0,0 +1,45 @@
+<?php 
+
+// class ContohStatic{
+//     public static $angka = 1;
+
+//     public static function halo(){
+//         return "halo ". self::$angka++ ." kali";
+//     }
+
+
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+//Static Keyword :terikat dengan kelas, nilai objek tetap meski di instansi berkali kali
+
+
+Class Contoh{
+    public static $angka = 1;
+
+    public function halo(){
+        return "halo " . self::angka++ ."Kali. <br>";
+    }
+}
+
+$obj = new Contoh();
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
+echo "<hr>";
+
+$obj2 = new Contoh();
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
+
+
+//objek akan melanjutkan hitungan walaupun beda objek
+
+?>
