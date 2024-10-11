a2226dbfa887757470dac6e773513065a665c996
DaffaMuhamad <Daffamuhamadra@gmail.com>
2024-10-11 21:51:27 +0700

    Video #16 OOP : Autoloading

---
+++ OOP/Video#16/Autoloading/index.php
@@ -0,0 +1,15 @@
+<?php 
+require_once("App/init.php");
+
+$produk1 = new komik("Naruto","Masashi Kishimoto","Shonen Jump",30000, 11);
+$produk2 = new game("Uncharted","Neil Druckmann","Sony Computer",25000, 50);
+
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+
+new Coba();
