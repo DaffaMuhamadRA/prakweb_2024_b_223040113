606f15fb6874c15a07779390e39780c7515958ac
DaffaMuhamad <Daffamuhamadra@gmail.com>
2024-10-19 23:36:11 +0700

    Video #8 MVC : Database Wrapper

---
+++ MVC/Video8/app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+define ("BASEURL","http://localhost/PraktikumWeb2/MVC/Video8/public/");
+
+//DB
+
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","phpmvc");
